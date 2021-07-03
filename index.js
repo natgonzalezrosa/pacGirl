8fcfad953fb0465e683a9d43e8a373ad3d18f39a
natgonzalezrosa <natgonzalezrosa@gmail.com>
2021-07-02 22:03:45 -0400

    added more logic to game

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ function createBoard() {
             squares[i].classList.add('pac-dot')
         } else if (layout[i] === 1) {
             squares[i].classList.add('wall')
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('ghost-lair')
         } else if (layout[i] === 3) {
             squares[i].classList.add('power-pellet')
         }
@@ -73,23 +75,45 @@ let pacmanCurrentIndex = 490
 squares[pacmanCurrentIndex].classList.add('pacman')
 
 function control(e) {
+    squares[pacmanCurrentIndex].classList.remove('pacman')
     switch(e.keyCode) {
         case 40:
         console.log('pressed down')
-        
+        if (
+            !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+            pacmanCurrentIndex + width < width * width
+            ) 
+            pacmanCurrentIndex += width
         break
         case 38:
         console.log('pressed up')
-        
+        if (
+            !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
+            pacmanCurrentIndex - width >=0
+            ) 
+            pacmanCurrentIndex -= width
         break
         case 37: 
         console.log('pressed left')
-        
+        if( 
+            !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+            pacmanCurrentIndex % width !==0
+            ) 
+            pacmanCurrentIndex -=1
         break
         case 39:
         console.log('pressed right')
-        
+        if(
+            !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+            pacmanCurrentIndex % width < width -1
+            ) 
+            pacmanCurrentIndex +=1
         break
     }
+    squares[pacmanCurrentIndex].classList.add('pacman')
 }
 document.addEventListener('keyup', control)
